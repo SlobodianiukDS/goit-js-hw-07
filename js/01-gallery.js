ba788a24ad91239f0834609070e6f4904ff48137
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-11-21 01:05:06 +0200

    создает галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryPlace = document.querySelector(`.gallery`);
+
 console.log(galleryItems);
+
+const changeGalleryOfImage = galleryItems.map((galleryItem) => {
+  const changeItemEl = document.createElement(`a`);
+  changeItemEl.href = galleryItem.original;
+  changeItemEl.classList.add(`gallery__link`);
+
+  const changeImgEl = document.createElement(`img`);
+  changeImgEl.classList.add(`gallery__image`);
+  changeImgEl.src = galleryItem.preview;
+  changeImgEl.alt = galleryItem.description;
+  changeImgEl.dataset.source = galleryItem.original;
+
+  changeItemEl.appendChild(changeImgEl);
+
+  return changeItemEl;
+});
+
+galleryPlace.append(...changeGalleryOfImage);
