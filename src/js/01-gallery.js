4d7d69e096870e72121fe1f627935e53a9d5e21a
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-11-21 23:52:58 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,11 @@
 import { galleryItems } from './gallery-items.js';
+const basicLightbox = require('basiclightbox');
+import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
 const galleryPlace = document.querySelector(`.gallery`);
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const changeGalleryOfImage = galleryItems.map(galleryItem => {
   const changeItemEl = document.createElement(`a`);
@@ -27,6 +29,23 @@ galleryPlace.addEventListener(`click`, callModalWindow);
 
 function callModalWindow(event) {
   event.preventDefault();
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+  // console.log(event.target.dataset.source);
 
-  console.log();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800">
+`);
+
+  instance.show();
 }
+document.addEventListener('keydown', event => {
+  const faidModal = document.querySelector(`div.basicLightbox`);
+  if (event.key === 'Escape' && faidModal !== null) {
+    faidModal.classList.remove(`basicLightbox--visible`);
+    setTimeout(() => {
+      faidModal.remove();
+    }, 500);
+  }
+});
